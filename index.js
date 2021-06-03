4402df76fa258372370404f62bfe49e6f69ff602
Austin Carman <abcarman12@gmail.com>
2021-06-03 11:17:25 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hey!!!');
+require('dotenv').config()
+const express = require('express')
+const server = express()
+
+console.log(process.env.USER); // env USER=austincarman
+console.log(process.env.SHELL); //env SHELL=/bin/zsh
+
+if(process.env.NODE_ENV === 'production') {
+    console.log('this means this code is deployed');
+}
+
+const PORT = process.env.PORT || 5000;
+
+console.log('port:', PORT);
+
+server.get('/api', (req, res) => {
+    res.json({ message: `${process.env.COHORT} ROCKS!` })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+})
